d0b72a48a8aab7af7971c21d28c5740d7d4b8c43
Derek Haller <dhaller.personal@gmail.com>
2018-01-28 19:29:29 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".") == true
   end
 
   def question?
-
+    self.end_with?("?") == true
   end
 
   def exclamation?
-
+    self.end_with?("!") == true
   end
 
   def count_sentences
-
+    punct_array = self.scan(/\b[\.\!\?]/)
+    punct_array.length
   end
 end
