2cbfdb21dd57126cbe02d91464afa3b06838c3e5
Derek Haller <dhaller.personal@gmail.com>
2018-01-28 19:18:13 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,7 +15,7 @@ class String
   end
 
   def count_sentences
-    punct_array = self.select { |i| i = ("."||"?"||"!")
+    punct_array = self.select { |i| i = ("."||"?"||"!") }
     punct_array.length
   end
 end
